4a2a909912fcd9d856a6b2d27f21d800fe4bb7d6
RamonPontes <rgdpontes@yahoo.com>
2025-07-28 14:50:11 -0300

    refactor: input handling to Entradas class for all operations

--- src/Multiplicacao.java
+++ src/Multiplicacao.java
@@ -12,14 +12,9 @@ import java.util.Scanner;
  */
 public class Multiplicacao {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Entradas.getInput();
         
-        System.out.println("Digite o numero 1: ");
-        int n1 = scanner.nextInt();
-        System.out.println("Digite o numero 2: ");
-        int n2 = scanner.nextInt();
-        
-        int result = n1 * n2;
+        int result = Entradas.getN1() * Entradas.getN2();
         
         System.out.println("Resultado: " + result);
     }
